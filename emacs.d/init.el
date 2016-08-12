b33fb57f2c135ca448c0856b8b93f25717ff5eb3
Spenser Pothier <spenser.pothier@nordstrom.com>
2016-08-12 12:35:40 -0700

    First steps at organizing emacs settings using orgmode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -42,23 +42,6 @@ Return a list of installed packages or nil for every skipped package."
 ;; Powerline evilmode
 (powerline-evil-vim-color-theme)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Helm stuff
-(require 'helm-config)
-(helm-mode t)
-(helm-projectile-on)
-
-;; Use helm for buffer navigation
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-
-;; Use helm for bookmarks
-(global-set-key (kbd "C-x r b") 'helm-bookmarks)
-;; Use helm for M-x commands
-(global-set-key (kbd "C-x m") 'helm-M-x)
-
-;; Tabcomplete in find file
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,16 +55,39 @@ Return a list of installed packages or nil for every skipped package."
 ;;(menu-bar-mode -1)
 (toggle-scroll-bar -1)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Org mode
-;;(org-mode t)
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(
-   (python . t)
-   (ruby . t)
-   (sh . t)
-   (plantuml . t)
-   ))
-(setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
-(setq backup-directory-alist `(("." . "~/.saves")))
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir "~/source/dotfiles/emacs.d")
+;; (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" dotfiles-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+  (require 'org-install)
+  (require 'ob-tangle))
+(package-initialize nil)
+(setq package-enable-at-startup nil)
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+;;; init.el ends here
